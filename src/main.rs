fbc3f54294c49bf9e27fcc5df547d909526893db
Arekusei <kotalexey@gmail.com>
2021-12-14 17:14:13 +0600

    изменить Cacher так, чтобы он сохранял HashMap

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,64 @@
 use std::thread;
 use std::time::Duration;
+use std::collections::HashMap;
 
 fn main() {
     let simulated_user_specified_value = 10;
-    let simulated_random_number = 7;
+    let simulated_random_number = 3;
 
     generate_workout(simulated_user_specified_value, simulated_random_number);
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_closure = |num| {
+    let mut expensive_result = Cacher::new(|num| {
         println!("Calculating slowly...");
         thread::sleep(Duration::from_secs(2));
         num
-    };
+    });
 
     if intensity < 25 {
-        println!("Today do {} pushups", expensive_closure(intensity));
-        println!("Next do {} situps", expensive_closure(intensity));
+        println!("Today do {} pushups", expensive_result.value(intensity));
+        println!("Next do {} situps", expensive_result.value(intensity));
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated!");
         } else {
-            println!("Today, run for {} minutes", expensive_closure(intensity));
+            println!("Today, run for {} minutes", expensive_result.value(intensity));
         }
     }
 }
 
 struct Cacher<T> where T: Fn(u32) -> u32, {
     calculation: T,
-    value: Option<u32>,
+    value: HashMap<u32, T>, //Option<u32>,
 }
 
 impl<T> Cacher<T> where T: Fn(u32) -> u32, {
     fn new(calculation: T) -> Cacher<T> {
         Cacher {
             calculation,
-            value: None,
+            value: HashMap::new(), //None,
         }
     }
 
     fn value(&mut self, arg: u32) -> u32 {
-        match self.value {
-            Some(v) => v,
-            None => {
-                let v = (self.calculation)(arg);
-                self.value = Some(v);
-                v
-            }
-        }
+        arg
+        // match self.value {
+        //     Some(v) => v,
+        //     None => {
+        //         let v = (self.calculation)(arg);
+        //         self.value = Some(v);
+        //         v
+        //     }
+        // }
     }
+}
+
+#[test]
+fn test_casher() {
+    let mut c = Cacher::new(|a| a);
+
+    let v1 = c.value(1);
+    let v2 = c.value(2);
+    assert_eq!(v2, 2);
 }
